a66f28d54f2126bccc5cab76b93a66fb41eed958
bumple <81680183+bumple@users.noreply.github.com>
2021-04-07 16:38:08 +0700

    first commit

---
+++ myjs.js
@@ -0,0 +1,18 @@
+let userName = prompt("Nhập tên người dùng")
+
+if ( userName === 'Admin') {
+
+}else if (userName == null) {
+    alert('Canceled');
+}else {
+    alert("I don't know you");
+};
+
+let pass = prompt("Nhập mật khẩu");
+if ( pass === "thanhdeptrai") {
+    alert("Welcome anh Thành!");
+}else if (pass === null){
+    alert('Thằng nào cố mò pass vậy ?');
+}else {
+    alert('Thằng nào cố mò pass vậy ?');
+}
